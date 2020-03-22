f5ab382fbc244d1e743d76e793f50de2a6820c40
Arkadiusz Zygan <a.zygan@gmail.com>
2020-03-22 23:39:01 +0100

    make it better

---
+++ client/build/precache-manifest.dd221a54ca5903ab8244140d7497c87b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9f7d23b0db65664b877caa79a393d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "98e863890e80c1e5fdfa",
+    "url": "./static/css/main.6c0e1812.chunk.css"
+  },
+  {
+    "revision": "316f23dc2de24b99924f",
+    "url": "./static/js/2.ab98745d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.ab98745d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e863890e80c1e5fdfa",
+    "url": "./static/js/main.dcef10d4.chunk.js"
+  },
+  {
+    "revision": "37f40108c5e311e475fa",
+    "url": "./static/js/runtime-main.32b701c7.js"
+  },
+  {
+    "revision": "0cba56738a0b7e83148cf67cc8a28e65",
+    "url": "./static/media/bcd.0cba5673.png"
+  }
+]);
