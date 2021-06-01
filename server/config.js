84a56d8b0b6b550658457b975866cc92ac4f79a0
Arkadiusz Zygan <a.zygan@gmail.com>
2021-06-01 10:27:46 +0200

    Change in db

--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   // PORT: 8000,
-  DB: process.env.DB ? process.env.DB : 'mongodb+srv://zyar3k:haslomongoDB@cluster0-y9wgt.mongodb.net/CraftShopDB'
+  DB: process.env.DB
+    ? process.env.DB
+    : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-y9wgt.mongodb.net/CraftShopDB`,
 };
--- server/server.js
+++ server/server.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./config');
-const productRoutes = require('./routes/product.routes');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
+const productRoutes = require("./routes/product.routes");
 
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', productRoutes);
+app.use("/api", productRoutes);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
-
-mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(config.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 let db = mongoose.connection;
 
-db.once('open', () => console.log('Wow!! You\'re connected to the database!'));
-db.on('error', (error) => console.log('Error ' + error));
-
+db.once("open", () => console.log("Wow!! You're connected to the database!"));
+db.on("error", (error) => console.log("Error " + error));
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port: `, config.PORT);
