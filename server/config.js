31df7146bb519ad01f38bf83dfb4c9ceba18ab60
Arkadiusz Zygan <a.zygan@gmail.com>
2021-06-01 10:33:31 +0200

    Make it better

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // PORT: 8000,
   DB: process.env.DB
     ? process.env.DB
     : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-y9wgt.mongodb.net/CraftShopDB`,
